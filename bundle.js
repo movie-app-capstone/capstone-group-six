6422ca1f2e6318f940f65798cae94a2d6720c6cc
Katie Vargas <you@example.com>
2024-04-15 21:54:39 -0400

    modified Trivia.html and css

--- bundle.js
+++ bundle.js
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import questions from './questions.json';
 import StatBar from './components/StatBar';
@@ -7,14 +6,12 @@ import Reset from './components/Rest';
 import Answer_module from './components/Answer.module.scss';
 import Classnames from 'classnames';
 import styles from './App.module.scss';
-
-function App() {
+const App = () => {
     const allQuestions = questions;
     const [currentQuestionIdx, setCurrentQuestionIdx] = useState(0);
     const [correctAnswers, setCorrectAnswers] = useState(0);
     const [incorrectAnswers, setIncorrectAnswers] = useState(0);
     const [waitingToAdvance, setWaitingToAdvance] = useState(false);
-
     const onSubmit = (correct) => {
         if (correct)
             setCorrectAnswers(correctAnswers + 1);
@@ -22,52 +19,22 @@ function App() {
             setIncorrectAnswers(incorrectAnswers + 1);
         setWaitingToAdvance(true);
     };
-
     const advance = () => {
         setWaitingToAdvance(false);
         setCurrentQuestionIdx(currentQuestionIdx + 1);
     };
-
     const reset = () => {
         setCurrentQuestionIdx(0);
         setCorrectAnswers(0);
         setIncorrectAnswers(0);
         setWaitingToAdvance(false);
     };
-
     if (currentQuestionIdx >= allQuestions.questions.length)
-        return (
-            <Reset
-                totalQuestions={allQuestions.questions.length}
-                correctQuestions={correctAnswers}
-                onPress={reset}
-            />
-        );
-
-    return (
-        <div>
-            <div className={styles.app}>
-                <StatBar
-                    currentQuestion={currentQuestionIdx + 1}
-                    totalQuestions={allQuestions.questions.length}
-                    correct={correctAnswers}
-                    incorrect={incorrectAnswers}
-                />
-                <QuestionComp
-                    question={allQuestions.questions[currentQuestionIdx]}
-                    onSubmit={onSubmit}
-                />
-                {true && (
-                    <button
-                        onClick={advance}
-                        className={Classnames(Answer_module.answer, styles['next-button'])}
-                    >
-                        Next Question...
-                    </button>
-                )}
-            </div>
-        </div>
-    );
-}
-
-export default App; // Export the App component
+        return (React.createElement(Reset, { totalQuestions: allQuestions.questions.length, correctQuestions: correctAnswers, onPress: reset }));
+    return (React.createElement("div", null,
+        React.createElement("div", { className: styles.app },
+            React.createElement(StatBar, { currentQuestion: currentQuestionIdx + 1, totalQuestions: allQuestions.questions.length, correct: correctAnswers, incorrect: incorrectAnswers }),
+            React.createElement(QuestionComp, { question: allQuestions.questions[currentQuestionIdx], onSubmit: onSubmit }),
+            true && (React.createElement("button", { onClick: advance, className: Classnames(Answer_module.answer, styles['next-button']) }, "Next Question...")))));
+};
+export default App;
